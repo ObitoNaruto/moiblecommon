5ca15c61c985c0caad7356da270640846173701e
冀秦 <xinming.xxm@alibaba-inc.com>
2016-03-27 15:44:14 +0800

    view utils

--- utils/src/main/java/com/android/mobile/utils/util/ViewUtils.java
+++ utils/src/main/java/com/android/mobile/utils/util/ViewUtils.java
@@ -1,7 +1,123 @@
 package com.android.mobile.utils.util;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Field;
+
 /**
  * Created by xinming.xxm on 2016/3/27.
  */
 public class ViewUtils {
+
+    /**
+     * 通过遍历得到每个子view的高度得到整个AbsListView的高度
+     * @param view
+     * @return
+     */
+    public static int getAbsListViewHeightBasedOnChildren(AbsListView view) {
+        ListAdapter adapter;
+        if (view == null || (adapter = view.getAdapter()) == null) {
+            return 0;
+        }
+
+        int height = 0;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View item = adapter.getView(i, null, view);
+            if (item instanceof ViewGroup) {
+                item.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            item.measure(0, 0);
+            height += item.getMeasuredHeight();
+        }
+        height += view.getPaddingTop() + view.getPaddingBottom();
+        return height;
+    }
+
+    /**
+     * 通过遍历每个子view的高度计算达到整个ListView的高度
+     * @param view
+     * @return
+     */
+    public static int getListViewHeightBasedOnChildren(ListView view) {
+        int height = getAbsListViewHeightBasedOnChildren(view);
+        ListAdapter adapter;
+        int adapterCount;
+        if (view != null && (adapter = view.getAdapter()) != null && (adapterCount = adapter.getCount()) > 0) {
+            height += view.getDividerHeight() * (adapterCount - 1);
+        }
+        return height;
+    }
+
+    /**
+     * 获取GridView的垂直间距
+     * @param view
+     * @return
+     */
+    public static int getGridViewVerticalSpacing(GridView view) {
+        // get mVerticalSpacing by android.widget.GridView
+        Class<?> demo = null;
+        int verticalSpacing = 0;
+        try {
+            demo = Class.forName("android.widget.GridView");
+            Field field = demo.getDeclaredField("mVerticalSpacing");
+            field.setAccessible(true);
+            verticalSpacing = (Integer)field.get(view);
+            return verticalSpacing;
+        } catch (Exception e) {
+            /**
+             * accept all exception, include ClassNotFoundException, NoSuchFieldException, InstantiationException,
+             * IllegalArgumentException, IllegalAccessException, NullPointException
+             */
+            e.printStackTrace();
+        }
+        return verticalSpacing;
+    }
+
+    /**
+     * 动态设置view高度
+     * @param view
+     * @param height
+     */
+    public static void setViewHeight(View view, int height) {
+        if (view == null) {
+            return;
+        }
+
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.height = height;
+    }
+
+    /**
+     * 动态设置view的宽度
+     * @param view
+     * @param width
+     */
+    public static void setViewWidth(View view, int width){
+        if(null == view){
+            return;
+        }
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.width = width;
+    }
+
+    /**
+     * 设置ListView高度
+     * @param view
+     */
+    public static void setListViewHeightBasedOnChildren(ListView view) {
+        setViewHeight(view, getListViewHeightBasedOnChildren(view));
+    }
+
+    /**
+     * 设置AbsListView高度
+     * @param view
+     */
+    public static void setAbsListViewHeightBasedOnChildren(AbsListView view) {
+        setViewHeight(view, getAbsListViewHeightBasedOnChildren(view));
+    }
 }
